d891fb6329a4ae090dbf3f82c0cd7ed6e749def0
Karthik <gvkarthik93@gmail.com>
2018-03-04 13:05:26 -0500

    Changing from httpserver to Tornado REST server

--- appserver.py
+++ appserver.py
@@ -1,102 +1,32 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import socketserver
 import os
-import http.server
-from os import curdir, sep
-import mimetypes
-import cgi
-import threading
-import sqlite3
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
 import json
-#from validate import UserValidation
-
-PORT = int(os.environ.get('PORT', 8080))
-
-class myHandler(BaseHTTPRequestHandler):
-    #Handler for GET requests
-    def do_GET(self):
-        if self.path=="/":
-            self.path="/index.html"
-
-        if self.path=="/index":
-            self.path="/index.html"
-
-        try:
-            #Open the static file requested and send it
-            #Check the file extension required
-            #Set the right mime type
-            #rb is used to read all the mime types and display them
-            f = open(curdir + sep + self.path, 'rb')
-            mimetype, _ = mimetypes.guess_type(self.path)
-            try:
-                self.send_response(200)
-                self.send_header('Content-type',mimetype)
-                self.end_headers()
-                self.wfile.write(f.read())
-                f.close()
-            except:
-                print ("Connection Aborted: Established connection has been dropped")
-            return
-        except IOError:
-            self.send_error(404,'File Not Found: %s' % self.path)
-
-    def do_POST(self):
-        if self.path == "/signup":
-            # Logic
-            dataVariable = self.headers['Content-Length']
-            #dataVariable contains the length of the variable provided in the string format
-            dataVar = int(dataVariable)
-            user_input = self.rfile.read(dataVar).decode().strip()
-            user_details = json.loads(user_input)
-            print ("user input: ", user_details)
-
-            if True:
-                #UserValidation.register(user_details)
-                print ("Registered User")
-                validation_response = {"User_Detail":"Valid"}
-                validation_response = json.dumps(validation_response)
-                print ("Sending Registration Confirmation")
-                self.send_response(200)
-                self.send_header("Content-type", "text/plain")
-                #self.send_header("Access-Control-Allow-Origin", "localhost:3000")
-                self.end_headers()
-                self.flush_headers()
-                self.wfile.write(validation_response.encode("utf-8"))
-            else:
-                print ("Invalid User")
+import tornado.escape
 
 
-        if self.path == "/login":
-            # Logic
-            dataVariable = self.headers['Content-Length']
-            #dataVariable contains the length of the variable provided in the string format
-            dataVar = int(dataVariable)
-            user_input = self.rfile.read(dataVar).decode().strip()
-            print ("Login Credentials: ", user_input)
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('./index.html')
 
+class LoginHandler(tornado.web.RequestHandler):
+    def post(self):
+        data = tornado.escape.json_decode(self.request.body)
+        print (data)
+        self.write(json.dumps({"A":1}))
 
-        if self.path == "/post_question":
-            # Logic
-            print ("post question")
-
-        if self.path == "/update":
-            # Logic
-            print ("Update Post")
-
-        if self.path == "/delete":
-            # Logic
-            print ("Delete Post")
-
-
-class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
-    pass
-
-try:
-    server = ThreadedTCPServer(('', PORT), myHandler)
-    print ('Started httpserver on port ' , PORT)
-    server.allow_reuse_address = True
-    server.serve_forever()
-
-except KeyboardInterrupt:
-    print ('CTRL + C RECEIVED - Shutting down the REST server')
-    server.socket.close()
+def main():
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/login", LoginHandler),
+        (r'/(.*)', tornado.web.StaticFileHandler, {'path': './'}),
+        (r"/image/*.png", tornado.web.StaticFileHandler, {'path':'./image/'}),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    port = int(os.environ.get("PORT", 8002))
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+ 
+if __name__ == "__main__":
+    main()
---
+++ testserver.py
@@ -0,0 +1,102 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+import os
+import http.server
+from os import curdir, sep
+import mimetypes
+import cgi
+import threading
+import sqlite3
+import json
+#from validate import UserValidation
+
+PORT = int(os.environ.get('PORT', 8080))
+
+class myHandler(BaseHTTPRequestHandler):
+    #Handler for GET requests
+    def do_GET(self):
+        if self.path=="/":
+            self.path="/index.html"
+
+        if self.path=="/index":
+            self.path="/index.html"
+
+        try:
+            #Open the static file requested and send it
+            #Check the file extension required
+            #Set the right mime type
+            #rb is used to read all the mime types and display them
+            f = open(curdir + sep + self.path, 'rb')
+            mimetype, _ = mimetypes.guess_type(self.path)
+            try:
+                self.send_response(200)
+                self.send_header('Content-type',mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+            except:
+                print ("Connection Aborted: Established connection has been dropped")
+            return
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        if self.path == "/signup":
+            # Logic
+            dataVariable = self.headers['Content-Length']
+            #dataVariable contains the length of the variable provided in the string format
+            dataVar = int(dataVariable)
+            user_input = self.rfile.read(dataVar).decode().strip()
+            user_details = json.loads(user_input)
+            print ("user input: ", user_details)
+
+            if True:
+                #UserValidation.register(user_details)
+                print ("Registered User")
+                validation_response = {"User_Detail":"Valid"}
+                validation_response = json.dumps(validation_response)
+                print ("Sending Registration Confirmation")
+                self.send_response(200)
+                self.send_header("Content-type", "text/plain")
+                #self.send_header("Access-Control-Allow-Origin", "localhost:3000")
+                self.end_headers()
+                self.flush_headers()
+                self.wfile.write(validation_response.encode("utf-8"))
+            else:
+                print ("Invalid User")
+
+
+        if self.path == "/login":
+            # Logic
+            dataVariable = self.headers['Content-Length']
+            #dataVariable contains the length of the variable provided in the string format
+            dataVar = int(dataVariable)
+            user_input = self.rfile.read(dataVar).decode().strip()
+            print ("Login Credentials: ", user_input)
+
+
+        if self.path == "/post_question":
+            # Logic
+            print ("post question")
+
+        if self.path == "/update":
+            # Logic
+            print ("Update Post")
+
+        if self.path == "/delete":
+            # Logic
+            print ("Delete Post")
+
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+try:
+    server = ThreadedTCPServer(('', PORT), myHandler)
+    print ('Started httpserver on port ' , PORT)
+    server.allow_reuse_address = True
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print ('CTRL + C RECEIVED - Shutting down the REST server')
+    server.socket.close()

e2e7cd6f22e9d3db1c9566d057410227dcd6b69d
Sihan Chen <csh.sihan@gmail.com>
2018-03-08 19:49:52 -0500

    adding relationships

--- database_setup.py
+++ database_setup.py
@@ -11,11 +11,23 @@ class EmailSetting(Base):
     id = Column(Integer, primary_key=True)
     type = Column(String(100), nullable=False)
 
-    # users = relationship("User", back_populates="email_setting")
+    users = relationship("User", back_populates="email_setting")
 
     def __repr__(self):
         return "<Email_setting(id='%s', type='%s')>" % (
                 self.id, self.type)
+
+
+class UserCourse(Base):
+    __tablename__ = 'user_course'
+
+    user_name = Column(String(100), ForeignKey("users.user_name"), primary_key=True)
+    course_id = Column(Integer, ForeignKey("courses.id"), primary_key=True)
+    role_id = Column(Integer, ForeignKey("role.id"), nullable=False)
+    
+    u_c = relationship("User", back_populates="course")
+    c_u = relationship("Course", back_populates="user")
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -26,10 +38,10 @@ class User(Base):
     email = Column(String(100), nullable=False) 
     email_setting_id = Column(Integer, ForeignKey("email_setting.id"))
 
-    # email_setting = relationship("EmailSetting", back_populates="users")
-    # courses = relationship("UserCourse", back_populates="users")
+    email_setting = relationship("EmailSetting", back_populates="users")
+    course = relationship("UserCourse", back_populates="u_c")
+    replies = relationship("Reply", back_populates="users")
 
-    
     def __repr__(self):
         return "<User(user_name='%s', password='%s', name='%s', email='%s')>" % (
                 self.user_name, self.name, self.password, self.email)
@@ -42,22 +54,12 @@ class Course(Base):
     school_name = Column(String(100), nullable=False)
     course_title = Column(String(100), nullable=False)
 
-    # users = relationship("UserCourse", back_populates="courses")
-    # post = relationship("Course", back_populates="posts")
+    user = relationship("UserCourse", back_populates="c_u")
+    posts = relationship("Course", back_populates="posts")
 
     def __repr__(self):
         return "<Course(course_name='%s', school_name='%s', course_title='%s')>" % (
                 self.course_name, self.school_name, self.course_title)
-
-class UserCourse(Base):
-    __tablename__ = 'user_course'
-
-    user_name = Column(String(100), ForeignKey("users.user_name"), primary_key=True)
-    course_id = Column(Integer, ForeignKey("courses.id"), primary_key=True)
-    role_id = Column(Integer, ForeignKey("role.id"), nullable=False)
-
-    # user = relationship("User", back_populates="courses")
-    # course = relationship("Course", back_populates="users")
 
 class Role(Base):
     __tablename__ = 'role'
@@ -76,7 +78,7 @@ class VisibilityType(Base):
     id = Column(Integer, primary_key=True)
     type = Column(String(100), nullable=False)
 
-    # post = relationship("Post", back_populates="visibility_type")
+    posts = relationship("Post", back_populates="visibility_type")
 
     def __repr__(self):
         return "<VisibilityType(id='%s', type='%s')>" % (
@@ -89,7 +91,7 @@ class PostType(Base):
     id = Column(Integer, primary_key=True)
     type = Column(String(100), nullable=False)
 
-    # post = relationship("Post", back_populates="visibility_type")
+    posts = relationship("Post", back_populates="post_type")
 
     def __repr__(self):
         return "<PostType(id='%s', type='%s')>" % (
@@ -104,42 +106,46 @@ class Post(Base):
     short_description = Column(String(500))
     description = Column(String(1000))
     create_time = Column(Time, nullable=False)
-    post_username = Column(String(100), ForeignKey("users.user_name"))
-    answerer = Column(String(100), ForeignKey("users.user_name"))
-    course_id = Column(Integer, ForeignKey("courses.id"))
-    post_type = Column(Integer, ForeignKey("post_type.id"))
-    visibility = Column(Integer, ForeignKey("visibility_type.id"))
     vote_count = Column(Integer, default=0)
+    post_username = Column(String(100), ForeignKey("users.user_name"))
+    answerer_username = Column(String(100), ForeignKey("users.user_name"))
+    course_id = Column(Integer, ForeignKey("courses.id"))
+    post_type_id = Column(Integer, ForeignKey("post_type.id"))
+    visibility_type_id = Column(Integer, ForeignKey("visibility_type.id"))
 
-    # post_author = relationship("User", use_list=False, foreign_keys=[post_username], back_populates="posts")
-    # answer_ta = relationship("User", use_list=False, foreign_keys=[answerer], back_populates="posts")
-    # course = relationship("Course", back_populates="posts")
-    # reply = relationship("Reply", back_populates="posts")
-
+    visibility_type = relationship("VisibilityType", back_populates="posts")
+    post_type = relationship("PostType", back_populates="posts")
+    course = relationship("Course", back_populates="posts")
+    replies = relationship("Reply", back_populates="posts")
+    
+    post_author = relationship("User", foreign_keys=[post_username], back_populates="posts")
+    answer_ta = relationship("User", foreign_keys=[answerer_username], back_populates="posts")
+    
     def __repr__(self):
         return "<Post(header='%s', short_description='%s', description='%s', \
                 create_time='%s', post_username='%s', answer='%s', vote_count='%s')>" % (
                     self.header, self.short_description, self.description, \
-                    self.create_time, self.post_username, self.answer, self.vote_count)
+                    self.create_time, self.post_username, self.answerer_username, 
+                    self.vote_count)
 
 class Reply(Base):
     __tablename__ = 'replies'
 
     id = Column(Integer, primary_key=True)
     post_id = Column(Integer, ForeignKey("posts.id"))    
+    username = Column(String(100), ForeignKey("users.user_name"))
     answer = Column(String(1000))
     create_time = Column(Time, nullable=False)
-    username = Column(String(100), ForeignKey("users.user_name"))
     vote_count = Column(Integer, default=0)
 
-    # post = relationship("Post", back_populates="replies")
-    # user = relationship("User", use_list=False, back_populates="users")
+    post = relationship("Post", back_populates="replies")
+    user = relationship("User", back_populates="users")
 
     def __repr__(self):
         return "<Reply(postid='%s', answer='%s', create_time='%s', \
                 username='%s', vote_count='%s')>" % ( 
-                    self.post_id, self.answer, self.create_time, \
-                    self.username, self.vote_count)
+                    self.post_id, self.answer, self.username, \
+                    self.create_time, self.vote_count)
 
 
 engine = create_engine('sqlite:///Zocalo.db')

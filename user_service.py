0d8ddaf5c9dfd820615441f7cf3ae669176f7d8d
Sihan Chen <csh.sihan@gmail.com>
2018-03-10 22:00:23 -0500

    new user script

---
+++ user_service.py
@@ -0,0 +1,60 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import MultipleResultsFound
+from database_setup import User, Base
+
+engine = create_engine('sqlite:///Zocalo.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class UserService:
+
+    def hash_password(self, pwd):
+        return pwd
+
+    def check_valid():
+        pass
+
+    def login(self, u_n, pwd):
+        try:
+            user = session.query(User).filter_by(user_name=u_n).one()
+        except NoResultFound:
+            return (-1, "No username founded")
+
+        return ("Success", "") if user.password == self.hash_password(pwd) \
+            else (-1, "Wrong password")
+
+
+    def register(self, info):
+        
+        # check validate
+
+        pwd = self.hash_password(info["password"])
+        new_user = User(
+            user_name=info["user_name"], 
+            password=pwd,
+            name=info["name"], 
+            email=info["email"]
+        )
+
+        session.add(new_user)
+        session.commit()
+        return (1, "Success")
+
+    def change_password(self, u_n, pwd): 
+        try:
+            user = session.query(User).filter_by(user_name=u_n).one()
+        except NoResultFound:
+            return (-1, "No username founded")
+
+        user.password = self.hash_password(pwd)
+        session.commit()
+        return (1, "Success")
+        
+
+    def reset_password(self):
+        pass
+
+    def check_admin(self):
+        pass

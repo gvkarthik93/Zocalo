44227ec2075963f0c19c237889541b08dd9d69f9
Karthik <gvkarthik93@gmail.com>
2018-03-26 15:43:44 -0400

    modifying end points

--- appserver.py
+++ appserver.py
@@ -24,74 +24,22 @@ class SignupHandler(tornado.web.RequestHandler):
         response = tempData()
         self.write(json.dumps(response))
 
-class PostNewQuestion(tornado.web.RequestHandler):
+class DisplayContent(tornado.web.RequestHandler):
     def post(self):
         data = tornado.escape.json_decode(self.request.body)
         response = tempData()
         self.write(json.dumps(response))
 
-class PostNewAnswer(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class GetSpecificPostContent(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class EditSpecificPost(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class DeleteSpecificPost(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class EditSpecificPostContent(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class DeleteSpecificPostContent(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class ChangePassword(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class ResetPassword(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
-
-class ChangeDisplayName(tornado.web.RequestHandler):
-    def post(self):
-        data = tornado.escape.json_decode(self.request.body)
-        response = tempData()
-        self.write(json.dumps(response))
+# /edits/Post
+# /edits/Content
+# /edits/Comments
 
 def main():
     application = tornado.web.Application([
         (r"/", MainHandler),
         (r"/login", LoginHandler),
         (r"/sign_up", SignupHandler),
-        (r"/post_details", GetPostContent),
-        (r"/change_password", ChangePassword),
-        (r"/reset_password", ResetPassword),
+        (r"/display", SignupHandler),
         (r'/(.*)', tornado.web.StaticFileHandler, {'path': './'}),
         (r"/image/*.png", tornado.web.StaticFileHandler, {'path':'./image/'}),
     ])

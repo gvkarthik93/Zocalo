3413b725fd14e4847773b70ec9e8110f88698e43
Sihan Chen <csh.sihan@gmail.com>
2018-03-28 01:13:00 -0400

    restructure folders

---
+++ db/database_setup.py
@@ -0,0 +1,168 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, UniqueConstraint, func
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class EmailSetting(Base):
+    __tablename__ = 'email_setting'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(100), nullable=False)
+
+    users = relationship("User", back_populates="email_setting")
+
+    def __repr__(self):
+        return "<Email_setting(id='%s', type='%s')>" % (
+                self.id, self.type)
+
+
+class UserCourse(Base):
+    __tablename__ = 'user_course'
+
+    user_name = Column(String(100), ForeignKey("users.user_name"), primary_key=True)
+    course_id = Column(Integer, ForeignKey("courses.id"), primary_key=True)
+    role_id = Column(Integer, ForeignKey("role.id"), nullable=False)
+    
+    u_c = relationship("User", back_populates="courses")
+    c_u = relationship("Course", back_populates="users")
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_name = Column(String(100), primary_key=True)
+    password = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False) 
+    email_setting_id = Column(Integer, ForeignKey("email_setting.id"))
+
+    email_setting = relationship("EmailSetting", back_populates="users")
+    courses = relationship("UserCourse", back_populates="u_c")
+    replies = relationship("Reply", back_populates="user")
+
+    def __repr__(self):
+        return "<User(user_name='%s', password='%s', name='%s', email='%s')>" % (
+                self.user_name, self.name, self.password, self.email)
+
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True)
+    course_name = Column(String(100), nullable=False)
+    school_id = Column(Integer, ForeignKey("schools.id"))
+    course_title = Column(String(100), nullable=False)
+
+    users = relationship("UserCourse", back_populates="c_u")
+    posts = relationship("Post", back_populates="course")
+
+    school = relationship("School", back_populates="courses")
+
+    def __repr__(self):
+        return "<Course(course_name='%s', school_id='%s', course_title='%s')>" % (
+                self.course_name, self.school_id, self.course_title)
+
+class School(Base):
+    __tablename__ = 'schools'
+
+    id = Column(Integer, primary_key=True)
+    school_name = Column(String(100), nullable=False)
+
+    courses = relationship("Course", back_populates="school")
+
+    def __repr__(self):
+        return "<School(id='%s', school_name='%s')>" % (
+                self.id, self.school_name)
+
+class Role(Base):
+    __tablename__ = 'role'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(100), nullable=False)
+
+    def __repr__(self):
+        return "<Role(id='%s', type='%s')>" % (
+                self.id, self.type)
+
+
+class VisibilityType(Base):
+    __tablename__ = 'visibility_type'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(100), nullable=False)
+
+    posts = relationship("Post", back_populates="visibility_type")
+
+    def __repr__(self):
+        return "<VisibilityType(id='%s', type='%s')>" % (
+                self.id, self.type)
+
+
+class PostType(Base):
+    __tablename__ = 'post_type'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(String(100), nullable=False)
+
+    posts = relationship("Post", back_populates="post_type")
+
+    def __repr__(self):
+        return "<PostType(id='%s', type='%s')>" % (
+                self.id, self.type)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    header = Column(String(100), nullable=False)
+    summary = Column(String(500))
+    description = Column(String(1000))
+    create_time = Column(DateTime, default=func.now())
+    tag = Column(String(100))
+    vote_count = Column(Integer, default=0)
+    post_username = Column(String(100), ForeignKey("users.user_name"))
+    answerer_username = Column(String(100), ForeignKey("users.user_name"))
+    course_id = Column(Integer, ForeignKey("courses.id"))
+    post_type_id = Column(Integer, ForeignKey("post_type.id"))
+    visibility_type_id = Column(Integer, ForeignKey("visibility_type.id"))
+
+    visibility_type = relationship("VisibilityType", back_populates="posts")
+    post_type = relationship("PostType", back_populates="posts")
+    course = relationship("Course", back_populates="posts")
+    replies = relationship("Reply", back_populates="post")
+    
+    post_author = relationship("User", foreign_keys=[post_username])
+    answer_ta = relationship("User", foreign_keys=[answerer_username])
+    
+    def __repr__(self):
+        return "<Post(header='%s', summary='%s', description='%s', \
+                create_time='%s', post_username='%s', answer='%s', vote_count='%s')>" % (
+                    self.header, self.summary, self.description, \
+                    self.create_time, self.post_username, self.answerer_username, 
+                    self.vote_count)
+
+class Reply(Base):
+    __tablename__ = 'replies'
+
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id"))
+    username = Column(String(100), ForeignKey("users.user_name"))
+    answer = Column(String(1000))
+    create_time = Column(DateTime, default=func.now())
+    vote_count = Column(Integer, default=0)
+
+    post = relationship("Post", back_populates="replies")
+    user = relationship("User", back_populates="replies")
+
+    def __repr__(self):
+        return "<Reply(postid='%s', answer='%s', create_time='%s', \
+                username='%s', vote_count='%s')>" % ( 
+                    self.post_id, self.answer, self.username, \
+                    self.create_time, self.vote_count)
+
+
+engine = create_engine('sqlite:///Zocalo.db')
+
+Base.metadata.create_all(engine)
---
+++ service/user_service.py
@@ -0,0 +1,70 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from sqlalchemy import create_engine
+from sqlalchemy import exists
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.exc import MultipleResultsFound
+from Zocalo.db.database_setup import *
+# from database_setup import User, Base
+
+engine = create_engine('sqlite:///../db/Zocalo.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class UserService:
+
+    def hash_password(self, pwd):
+        return pwd
+
+    def check_valid(self, input_email):
+        #use exist because we are just trying to find if input_email exist already, can switch back to try, except clause
+        exist = session.query(exists().where(User.email==input_email)).scalar()
+        if exist:
+            return (1, "This e-mail is already in the database")
+        else:
+            return(0, "This e-mail is not in the database")
+
+    def login(self, u_n, pwd):
+        try:
+            user = session.query(User).filter_by(user_name=u_n).one()
+        except NoResultFound:
+            return (0, "No username founded")
+        except MultipleResultsFound:
+            pass
+
+        return (1, "Success") if user.password == self.hash_password(pwd) \
+            else (0, "Wrong password")
+
+
+    def register(self, info):
+        
+        # check validate
+
+        pwd = self.hash_password(info["password"])
+        new_user = User(
+            user_name=info["user_name"], 
+            password=pwd,
+            name=info["name"], 
+            email=info["email"]
+        )
+
+        session.add(new_user)
+        session.commit()
+        return (1, "Success")
+
+    def change_password(self, u_n, pwd): 
+        try:
+            user = session.query(User).filter_by(user_name=u_n).one()
+        except NoResultFound:
+            return (0, "No username founded")
+        except MultipleResultsFound:
+            pass
+
+        user.password = self.hash_password(pwd)
+        session.commit()
+        return (1, "Success")   
+
+    def forget_password(self):
+        pass

c7103ce2d1c6c97b2f326f2776ab11001a9eb335
Sihan Chen <csh.sihan@gmail.com>
2018-04-01 18:45:37 -0400

    updating .gitignore

--- database/lookup_data.py
+++ database/lookup_data.py
@@ -1,6 +1,3 @@
-# import sys
-# import os
-# sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import *

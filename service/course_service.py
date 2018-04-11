9ceb5cbce93e6590d3945bb5c0aefbba5d1a54b4
mileslaw95 <milesklaw@yahoo.ca>
2018-04-11 00:29:23 -0400

    updated course_service

--- service/course_service.py
+++ service/course_service.py
@@ -14,44 +14,79 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 class CourseService:
-    def createCourse(self, c_name, s_name, c_title, c_id):
-        #might have to create a table for schools, so we know whether or not it is a valid school before creating a course 
-        #check if c_id is already used
-        course_exist = session.query(exists().where(Course.id == c_id)).scalar()
-        if course_exist:
-            return(0, "Error: This course already exist in db")
-        else:
+
+    #data: c_name, s_id, c_title
+    #input: course_name, school_id, course_title
+    #output: status, message
+    def createCourse(self, data):
+        try:
+            #check if the course already exist for that school
+            if session.query(
+                exists().where(and_(Course.school_id==data["s_id"], \
+                    Course.course_name==data["c_name"]))).scalar():
+                return {"status":0, "message":"Course name already exists for this school"}
+            #check if course title already exist for that school
+            if session.query(
+                exists().where(and_(Course.school_id==data["s_id"], \
+                    Course.course_title==data["c_title"]))).scalar():
+                return {"status":0, "message":"Course title already exists for this school"}
+
             new_course = Course(
-                id = c_id,
-                course_name = c_name,
-                school_name = s_name,
-                course_title = c_title
+                course_name = data["c_name"],
+                school_id = data["s_id"],
+                course_title = data["c_title"]
             )
-            session.add(new_course)
-            session.commit()
-            return(1,"Success")
-    
-    def enrollUser(self, u_name, c_id, r_id):
-        
-        #check if user is in user table
-        user_exist = session.query(exists().where(User.user_name == u_name)).scalar()
-        course_exist = session.query(exists().where(Course.id == c_id)).scalar() 
-        #check if course_id exists
-        
-        if user_exist and course_exist:        
-            new_enrollment = UserCourse(
-                user_name = u_name,
-                course_id = c_id,
-                role_id = r_id
-            )
-            session.add(new_enrollment)
-            session.commit()
-            return(1,"Success")
-        elif user_exist == False and course_exist == False:
-            return(0, "Error: User and Course not in DB")
-        elif user_exist == False:
-            return(0, "Error: User not in DB")
-        else:
-            return(0, "Error: Course not in DB")
-        
-        
+        except KeyError:
+                return {"status":0, "message":"Invalid JSON field"}
+
+        session.add(new_course)
+        session.commit()
+        return {"status":1, "message":"Course added!"}
+
+    #data: u_name
+    #input: username
+    #output: status, message, course_list
+    def getCourse(self,data):
+        #get school_id of user
+        try:
+            user = session.query(User).\
+                filter_by(user_name=data["u_name"]).one()
+            sch_id = user.school_id
+        except NoResultFound:
+            return {"status":0, "message":"User not founded", "course_list":[]}
+        except MultipleResultsFound:
+            print("should not happen")
+
+        #find courses associated with school_id    
+        try:
+            courses = session.query(Course).\
+                filter_by(school_id=sch_id).one()
+        except NoResultFound:
+            return {"status":0, "message":"No courses found for this school", "course_list":[]}
+        except MultipleResultsFound:
+            print("should not happen")
+
+        course_list = []
+        for c in courses:
+            d = {}
+            d["course_id"] = c.id
+            d["course_name"] = c.course_name
+            course_list.append(d)
+
+        return {"status":1, "message":"Success", "course_list":course_list}
+
+    #data: u_name, c_id
+    #input: username, course
+    #add user,course to userCourse table 
+    #output: status, message
+    #assume user and course_id are valid inputs
+    def enrollStudent(self,data):
+        #role_id set to 1 because enrollUser is for enrolling students
+        new_enrollment = UserCourse(
+            user_name = data["u_name"],
+            course_id = data["c_id"],
+            role_id = 1
+        )
+        session.add(new_enrollment)
+        session.commit()
+        return {"status":1, "message":"Success"}

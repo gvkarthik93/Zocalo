dcf8bf1cb5706a545c72baf9487141154d771c6b
SihanC <csh.sihan@gmail.com>
2018-04-23 16:42:53 -0400

    Disable token expiration for testing purpose

--- util/auth_util.py
+++ util/auth_util.py
@@ -2,30 +2,31 @@ import jwt
 import json
 from datetime import datetime, timedelta
 
-# Metadata for token
+# metadata for token
 JWT_SECRET = 'secret'
 JWT_ALGORITHM = 'HS256'
 JWT_EXP_MINUTES = 20
 
+
 class AuthUtil:
     def checkToken(self, auth_header):
         if auth_header is None or not auth_header.startswith('Bearer '):
-            return (0, json.dumps({"status":0, "message":"Missing Token"}))
+            return (0, json.dumps({"status": 0, "message": "Missing Token"}))
 
         jwt_token = auth_header.split(" ")[1]
         if jwt_token:
             try:
-                payload = jwt.decode(jwt_token, JWT_SECRET, 
+                payload = jwt.decode(jwt_token, JWT_SECRET,
                                      algorithms=[JWT_ALGORITHM])
             except (jwt.DecodeError, jwt.ExpiredSignatureError):
                 return (0, json.dumps(
-                {"status":0, "message":"Invalid Token"}))
+                    {"status": 0, "message": "Invalid Token"}))
         return (1, payload)
 
     def generateToken(self, username):
         print(datetime.now())
         payload = {
-            'username': username,
-            'exp': datetime.utcnow() + timedelta(minutes=JWT_EXP_MINUTES)
+            'username': username
+            # 'exp': datetime.utcnow() + timedelta(minutes=JWT_EXP_MINUTES)
         }
         return jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)

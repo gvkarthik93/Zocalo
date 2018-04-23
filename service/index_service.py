078fe9a3fa0fa7cba970d7446c4bcca6a0029419
Karthik <gvkarthik93@gmail.com>
2018-04-23 19:13:41 -0400

    Building Index

--- service/index_service.py
+++ service/index_service.py
@@ -1,22 +1,25 @@
 from user_service import UserService
 from post_service import PostService
 import pandas as pd
+import datetime
 
 class IndexSchema:
 	def __init__(self):
 		self.service = PostService()
 		self.dataFrame = None
+		self.lastUpdatedTime = None
 
 	def runTimeIndex(self, postService):
 		print ("Building runtime index")
-		data = service.get_questions()
+		data = service.get_all_questions()
 		self.dataFrame = pd.DataFrame(data)
+		self.lastUpdatedTime = datetime.datetime.now()
 
 	def updateIndex(self):
 		print ("Updating index")
-		data = service.get_questions_perquery()
+		data = service.get_all_questions_wtime(self.lastUpdatedTime)
 		df = pd.DataFrame(data)
-		# Update the self.dataframe here.
+		self.dataFrame.append(df)
 
 	def createIndex(self):
 		self.runTimeIndex()
